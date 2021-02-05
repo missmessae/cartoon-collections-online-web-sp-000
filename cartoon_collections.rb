2e7d3c739e34ebb6afa8f169db0cb1271ed00105
Annie Wiegel <annewiegel17@gmail.com>
2021-02-05 00:22:59 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,7 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  array.any?.length == 4
+  array.any? {|item| item == 4}
 end
 
 def find_the_cheese# code an argument here
